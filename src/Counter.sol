082750c12f377bc3ec744febb6e341323e7257b1
mlgarchery <martial.garchery@gmail.com>
2023-04-04 11:32:58 +0200

    using an external lib

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// this line breaks chisel when we import Counter
+import {DataTypes} from "@aave-v3-core/protocol/libraries/types/DataTypes.sol";
+
 contract Counter {
     uint256 public number;
 
